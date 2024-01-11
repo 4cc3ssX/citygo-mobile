4f5532df76caf215eb46f09a887fddf62b114dd4
4cc3ssX <4cc3ssflick@gmail.com>
2024-01-11 23:51:14 +0630

    chore: husky, eslint configs, and path alias setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,47 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['unused-imports', 'simple-import-sort'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+  overrides: [
+    // override "simple-import-sort" config
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // Side effect imports.
+              ['^\\u0000'],
+              // React and React Native packages come first.
+              ['^react', '^react-native', '^@?\\w'],
+              // Other libraries.
+              ['^.+'],
+              // Internal packages.
+              [
+                '^@(api|hooks|theme|assets|navigations|typescript|components|store|utils|configs|screens|helpers|locales|styles|constants|errors)(/.*|$)',
+              ],
+              // Parent imports. Put `..` last.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@api': './src/api',
+          '@hooks': './src/hooks',
+          '@theme': './src/theme',
+          '@assets': './src/assets',
+          '@navigations': './src/navigations',
+          '@typescript': './src/typescript',
+          '@components': './src/components',
+          '@store': './src/store',
+          '@utils': './src/utils',
+          '@configs': './src/configs',
+          '@screens': './src/screens',
+          '@helpers': './src/helpers',
+          '@locales': './src/locales',
+          '@styles': './src/styles',
+          '@constants': './src/constants',
+          '@errors': './src/errors',
+        },
+      },
+    ],
+  ],
 };
