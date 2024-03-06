47a2705388152678685f95b8f426574097f792df
4cc3ssX <4cc3ssflick@gmail.com>
2024-03-06 23:06:21 +0630

    chore: uat api endpoint

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,7 +1,7 @@
 export class Configs {
   static instance: Configs;
 
-  public static readonly BASE_URL: string = 'http://localhost:3000/api/v1';
+  public static readonly BASE_URL: string = 'https://citygo-app.vercel.app/api/v1';
 
   public static readonly AUTH_TOKEN_KEY: string = 'auth-token-key';
 
